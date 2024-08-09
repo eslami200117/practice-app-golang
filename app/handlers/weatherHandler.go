53c258a4e3032a56bd9fe0e242932719575c3a61
eslami <ab.eslami2001@gmail.com>
2024-08-09 18:53:46 +0330

    feat: add handler for user rec for process data

--- app/handlers/weatherHandler.go
+++ app/handlers/weatherHandler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"encoding/json"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"rest.gtld.test/realTimeApp/app/model"
 	"rest.gtld.test/realTimeApp/app/usecases"
@@ -29,4 +31,13 @@ func (w *weatherHandler) HandleWebSocketConnection(conn *websocket.Conn) {
 		json.Unmarshal(message, &weather)
 		w.WeatherUsecaseImp.WeatherDataProcessing(&weather)
 	}
+}
+
+func (w *weatherHandler) HnadleUserRecPrc(c *gin.Context) {
+	result := w.WeatherUsecaseImp.RainProccesin(0, 0)
+	c.JSON(http.StatusOK, gin.H{
+		"avr rain":   result,
+		"last value": w.WeatherUsecaseImp.LastValue(),
+	})
+
 }
