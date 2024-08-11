5eee66d5cb974510e2ce7871d2de4d815e10b3a1
eslami <ab.eslami2001@gmail.com>
2024-08-11 09:09:01 +0330

    feat: add function for user auth

--- app/repositories/weatherPostgresRepo.go
+++ app/repositories/weatherPostgresRepo.go
@@ -24,15 +24,21 @@ func (pr *WeatherPostgresRepo) InserWeatherData(data *entities.WeatherEntity) er
 	return nil
 }
 
-func (pr *WeatherPostgresRepo) Authen(in *model.Login) bool{
+func (pr *WeatherPostgresRepo) AuthenNode(in *model.Login) bool{
 	var node model.Node
 	pr.db.GetDb().First(&node, "username= ?", in.Username)
 	return node.Password == in.Password
 }
 
-func (pr *WeatherPostgresRepo) GetUser(username string, user *model.Login){
+func (pr *WeatherPostgresRepo) GetNode(username string, user *model.Login){
 	var node model.Node 
 	pr.db.GetDb().Where("username=?", username).Find(&node)
 	user.Password = node.Password
 	user.Username = node.Username
+}
+
+func (pr * WeatherPostgresRepo) AuthenUser(in *model.Login) bool {
+	var user model.User
+	pr.db.GetDb().First(&user, "username= ?", in.Username)
+	return user.Password == in.Password
 }
--- app/usecases/nodeUsecaseImp.go
+++ app/usecases/nodeUsecaseImp.go
@@ -19,13 +19,13 @@ func NewNodeImp(repo * repository.WeatherPostgresRepo) *NodeUsecaseImp{
 }
 
 func (nu *NodeUsecaseImp) AuthenticateNode(c *gin.Context, in *model.Login) bool{
-	result := nu.nodeRepo.Authen(in)
+	result := nu.nodeRepo.AuthenNode(in)
 	// if result{
 		// login, create lo
 	// }
 	return result
 }
 
-func (nu *NodeUsecaseImp) GetLoginUser(username string, user *model.Login){
-	nu.nodeRepo.GetUser(username, user)
+func (nu *NodeUsecaseImp) GetLoginNode(username string, user *model.Login){
+	nu.nodeRepo.GetNode(username, user)
 }
